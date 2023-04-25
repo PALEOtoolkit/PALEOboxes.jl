f6dab9b269035c804667f20d37e187a20e443936
sjdaines <stuart.daines@googlemail.com>
2023-04-25 10:37:46 +0100

    Change from import to using PrecompileTools

--- src/PALEOboxes.jl
+++ src/PALEOboxes.jl
@@ -27,7 +27,7 @@ using DocStringExtensions
 import OrderedCollections
 import Logging
 
-import PrecompileTools
+using PrecompileTools
 import TimerOutputs: @timeit, @timeit_debug
 
 include("utils/DocStrings.jl")
@@ -150,7 +150,7 @@ function run_model(model::Model; call_do_deriv=false, logger=Logging.NullLogger(
 end
 
 
-PrecompileTools.@setup_workflow begin
+@setup_workflow begin
     # create Reactions and register methods to precompile this code
 
     # Putting some things in `setup` can reduce the size of the
@@ -166,7 +166,7 @@ PrecompileTools.@setup_workflow begin
         "ReactionReservoirTotal", "ReactionUnstructuredVectorGrid", "ReactionCartesianGrid", "ReactionReservoirConst",
     ]
 
-    PrecompileTools.@precompile_workflow begin
+    @precompile_workflow begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
         for r in reactionlist
