a23018bf94f592025294f012e66d2a36c6e00d4d
sjdaines <stuart.daines@googlemail.com>
2024-03-19 08:43:56 +0000

    Fix doc for add_method_do_totals_default!

--- src/reactionmethods/VariableStatsMethods.jl
+++ src/reactionmethods/VariableStatsMethods.jl
@@ -1,8 +1,8 @@
 """
-    add_method_do_totals_default!(react::AbstractReaction; total_candidates=PB.get_variables(react)
+    add_method_do_totals_default!(react::AbstractReaction, total_candidates=PB.get_variables(react);
         [filterfn] [, methodname] [, total_localnames] [, operatorID]) 
 
-Create and add a method to add total variables (Scalar Properties), for Variables in `total_candidates` that match `filterfn`
+Create and add a method to add total variables (Scalar Properties), for Variables in collection `total_candidates` that match `filterfn`
 (defaults to those that are Array Variables and have attribute ``:calc_total == true`).
 
 NB: total Variables will require initialization to zero using [`add_method_initialize_zero_vars_default!`](@ref)
